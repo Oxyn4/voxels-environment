8d5bdc55d3f50d180005181793b41f3c8a6b51af
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-02-15 15:12:42 +0000

    added two new directory targets, certs for SSL certifcates and accounts for pki

---
+++ include/voxels/directories/voxels/certs.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <filesystem>
+
+#include <boost/program_options.hpp>
+
+#include <boost/log/trivial.hpp>
+
+namespace voxels::directories::voxels::logs
+{
+    const std::string LogsHomeFlag = "certs-home";
+
+    std::filesystem::path
+    Get(const boost::program_options::variables_map &VariableMap, const std::filesystem::path& StateHome) noexcept;
+}
